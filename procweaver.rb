b4cea5a8fef4ab173b03a81e07444bbd8732b880
Alex Nye <nyebismuth@gmail.com>
2012-12-15 18:28:50 -0800

    idea and design starting to come together

--- procweaver.rb
+++ procweaver.rb
@@ -1,9 +1,5 @@
 # get the numbers 0 - 15 in binary as all possible starting toggler states
 # add 2 at the end for the constant truth teller
-
-# generation could be simplified to just making arrays with 0-4 1s with the
-# rest being 0s
-
 starting_toggler_states = (1..4).map do |x|
   ([0] * (4 - x)) + ([1] * x) + [2]
 end
@@ -15,8 +11,6 @@ starting_toggler_states.each do |x|
     possible_configurations << y unless possible_configurations.index y
   end
 end
-
-possible_configurations.each do |x| puts x.join end
 
 def verify_answer
   puts "f"
@@ -34,13 +28,15 @@ def ask bar
   end
 end
 
-# ask 2 questions, each returning a boolean value for whether the result met
-# expectations or not.  Use inject to find the logical AND sum of the questions
-# and return when both are satisfied to indicate a succesfull question pair.
-# Actually should supply the sub array bar to question procs one by one and
-# they should return a set of indices along with a given known property of
-# elements under those indices so that another function may reason over them
-# to determine the final result of this particular test.
+=begin
+Ask 2 questions to current toggler/truth-teller configuration.
+Pass the results to constraint_applicator to determine candidate final result.
+Finally, verify answer.
+
+Each question proc should take an array and return a set of indices along with
+a given known property of the elements under those indices
+=end
+
 def take_questions pair
   return pair.inject(true) {|x| ask bar x}
 end
