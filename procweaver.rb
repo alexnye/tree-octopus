1b7d23d9c793e9b7f940c09eb0df3019be658143
Alex Nye <nyebismuth@gmail.com>
2012-12-15 16:47:49 -0800

    initial commit

---
+++ procweaver.rb
@@ -0,0 +1,44 @@
+# get the numbers 0 - 15 in binary as all possible starting toggler states
+# add 2 at the end for the constant truth teller
+starting_toggler_states = []
+(0..15).each do |x|
+  y = x.to_s 2
+  starting_toggler_states << (("0" * (4 - y.length)) + y).split('') + ["2"]
+end
+
+possible_configurations = []
+
+starting_toggler_states.each do |x|
+  x.permutation(5).each do |y|
+    possible_configurations << y unless possible_configurations.index(y)
+  end
+end
+
+def verify_answer
+  
+end
+
+# return each successful identification of the truth-teller
+def constraint_applicator
+  
+end
+
+# iterate through the permutations and ask questions using a block
+def ask bar
+  bar.each do |arr|
+    puts yield arr
+  end
+end
+
+# ask 2 questions, each returning a boolean value for whether the result met
+# expectations or not.  Use inject to find the logical AND sum of the questions
+# and return when both are satisfied to indicate a succesfull question pair.
+# Actually should supply the sub array bar to question procs one by one and
+# they should return a set of indices along with a given known property of
+# elements under those indices so that another function may reason over them
+# to determine the final result of this particular test.
+def take_questions pair
+  return pair.inject(true) {|x| ask bar x}
+
+# array of paired procs as questions iterated through and asked.
+# each encodes the expected 
